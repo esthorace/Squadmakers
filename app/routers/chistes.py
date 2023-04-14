539a41c5090e9408367bbe475eae228ad53c2e6e
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-04-14 20:58:05 -0300

    🚚 Rename:  data_chistes

--- app/routers/chistes.py
+++ app/routers/chistes.py
@@ -3,13 +3,17 @@ import random
 from fastapi import APIRouter, HTTPException, Query
 
 from ..controllers.API_solicitud import solicitud
-from ..data.data_chistes import URLS_CHISTES
+from .data.data_chistes import URLS_CHISTES
 
 router = APIRouter()
 
 
 @router.get("/chistes")
-async def get_chiste(query: str | None = Query(None, description="pokemon: Chuck o Dad")) -> dict:
+async def chiste(query: str | None = Query(None, description="pokemon: Chuck o Dad")) -> dict:
+    """
+    Recibe un parámetro, si es 'Chuk' o 'Dad', devuelve un chiste de un servicio API correspondiente,
+    si no recibe parámetro, devuelve un chiste al azar entre 'Chuk' o 'Dad'
+    """
     if query is None:
         query = random.choice(tuple(URLS_CHISTES))
 
