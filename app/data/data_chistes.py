1557d700ab84be9c360817e9edd7835ff4be8549
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-04-14 19:07:17 -0300

    Add: data_chistes

---
+++ app/data/data_chistes.py
@@ -0,0 +1,12 @@
+URLS_CHISTES = {
+    "Chuck": {
+        "url": "https://api.chucknorris.io/jokes/random",
+        "headers": {},
+        "claves": ["value"]
+    },
+    "Dad": {
+        "url": "https://icanhazdadjoke.com/",
+        "headers":  {"Accept": "application/json"},
+        "claves": ["joke"]
+    }
+}
--- app/routers/chistes.py
+++ app/routers/chistes.py
@@ -3,21 +3,9 @@ import random
 from fastapi import APIRouter, HTTPException, Query
 
 from ..controllers.API_solicitud import solicitud
+from ..data.data_chistes import URLS_CHISTES
 
 router = APIRouter()
-
-URLS_CHISTES = {
-    "Chuck": {
-        "url": "https://api.chucknorris.io/jokes/random",
-        "headers": {},
-        "claves": ["value"]
-    },
-    "Dad": {
-        "url": "https://icanhazdadjoke.com/",
-        "headers":  {"Accept": "application/json"},
-        "claves": ["joke"]
-    }
-}
 
 
 @router.get("/chistes")
