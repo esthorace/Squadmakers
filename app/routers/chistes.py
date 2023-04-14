24ea4395049687f5e52de13ce6cabbf15fc2254c
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-04-14 16:43:35 -0300

    Improve: chistes

--- app/routers/chistes.py
+++ app/routers/chistes.py
@@ -1,47 +1,36 @@
 import random
 
-import httpx
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
+
+from ..controllers.API_solicitud import solicitud
 
 router = APIRouter()
 
-chistes_tipo = {
+URLS_CHISTES = {
     "Chuck": {
         "url": "https://api.chucknorris.io/jokes/random",
         "headers": {},
-        "clave": "value"
+        "claves": ["value"]
     },
     "Dad": {
         "url": "https://icanhazdadjoke.com/",
         "headers":  {"Accept": "application/json"},
-        "clave": "joke"
+        "claves": ["joke"]
     }
 }
 
 
-def solicitud(url, headers, clave):
-    try:
-        response = httpx.get(url, headers=headers)
-    except httpx.RequestError:
-        raise HTTPException(
-            status_code=400, detail="Problemas en cargar datos de la API")
-    else:
-        resultado_dict: dict = response.json()
-        chiste = resultado_dict.get(clave)
-        return chiste
-
-
 @router.get("/chistes")
-async def get_chiste(param: str | None = None):
-    if param:
-        if param in chistes_tipo:
-            url, headers, clave = chistes_tipo[param].values()
-            chiste = solicitud(url, headers, clave)
-        else:
-            raise HTTPException(
-                status_code=400, detail=f"Parámetros esperados {[x for x in chistes_tipo]}, o ninguno'")
-    else:
-        azar = random.choice(tuple(chistes_tipo))
-        url, headers, clave = chistes_tipo[azar].values()
-        chiste = solicitud(url, headers, clave)
-    return {"chiste": chiste}
+async def get_chiste(query: str | None = Query(None, description="pokemon: Chuck o Dad")) -> dict:
+    if query is None:
+        query = random.choice(tuple(URLS_CHISTES))
+
+    if query not in URLS_CHISTES:
+        raise HTTPException(
+            status_code=400, detail=f"Parámetros esperados {[x for x in URLS_CHISTES]}, o ninguno'")
+
+    url = URLS_CHISTES[query]["url"]
+    headers = URLS_CHISTES[query]["headers"]
+    claves = URLS_CHISTES[query]["claves"]
+    chiste = solicitud(url=url, headers=headers, claves=claves)
+    return {"chiste": chiste, "pokemon": query}
